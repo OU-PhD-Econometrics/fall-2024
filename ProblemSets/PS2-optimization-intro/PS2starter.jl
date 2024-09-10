a3427ca7aa5dfb0f4685e1346ba63540037cb0de
Tyler Ransom <tyleransom@gmail.com>
2024-09-10 14:27:37 -0500

    update PS2 starter code

--- ProblemSets/PS2-optimization-intro/PS2starter.jl
+++ ProblemSets/PS2-optimization-intro/PS2starter.jl
@@ -1,7 +1,8 @@
+using Optim, DataFrames, CSV, HTTP, GLM
+
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
 # question 1
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
-using Optim
 f(x) = -x[1]^4-10x[1]^3-2x[1]^2-3x[1]-2
 minusf(x) = x[1]^4+10x[1]^3+2x[1]^2+3x[1]+2
 startval = rand(1)   # random starting value
@@ -13,9 +14,6 @@ println("min is ",Optim.minimum(result))
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
 # question 2
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
-using DataFrames
-using CSV
-using HTTP
 url = "https://raw.githubusercontent.com/OU-PhD-Econometrics/fall-2022/master/ProblemSets/PS1-julia-intro/nlsw88.csv"
 df = CSV.read(HTTP.get(url).body, DataFrame)
 X = [ones(size(df,1),1) df.age df.race.==1 df.collgrad.==1]
@@ -29,7 +27,6 @@ end
 beta_hat_ols = optimize(b -> ols(b, X, y), rand(size(X,2)), LBFGS(), Optim.Options(g_tol=1e-6, iterations=100_000, show_trace=true))
 println(beta_hat_ols.minimizer)
 
-using GLM
 bols = inv(X'*X)*X'*y
 df.white = df.race.==1
 bols_lm = lm(@formula(married ~ age + white + collgrad), df)
@@ -55,10 +52,12 @@ end
 #:::::::::::::::::::::::::::::::::::::::::::::::::::
 freqtable(df, :occupation) # note small number of obs in some occupations
 df = dropmissing(df, :occupation)
-df[df.occupation.==10,:occupation] .= 9
-df[df.occupation.==11,:occupation] .= 9
-df[df.occupation.==12,:occupation] .= 9
-df[df.occupation.==13,:occupation] .= 9
+df[df.occupation.==8 ,:occupation] .= 7
+df[df.occupation.==9 ,:occupation] .= 7
+df[df.occupation.==10,:occupation] .= 7
+df[df.occupation.==11,:occupation] .= 7
+df[df.occupation.==12,:occupation] .= 7
+df[df.occupation.==13,:occupation] .= 7
 freqtable(df, :occupation) # problem solved
 
 X = [ones(size(df,1),1) df.age df.race.==1 df.collgrad.==1]
